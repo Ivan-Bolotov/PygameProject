f048d6b65639d2d5a0b01da30d5c43f0d948c823
NeetAlinee <neetaline@yandex.ru>
2023-12-27 22:31:54 +0300

    Доработан класс Board

--- Classes/Board.py
+++ Classes/Board.py
@@ -22,3 +22,21 @@ class Board:
                           (i + 1) * self.cell_size + self.top,
                           self.cell_size, self.cell_size)
                 pygame.draw.rect(screen, "white", coords, width=1)
+
+    def get_cell(self, mouse_pos):
+        fk_x, fk_y = mouse_pos
+        x = fk_x - self.left
+        y = fk_y - self.top
+        if x // self.cell_size > self.width - 1 or y // self.cell_size > self.height - 1 or fk_y < self.top \
+                or fk_x < self.left:
+            return None
+        pos = (x // self.cell_size, y // self.cell_size)
+        return pos
+
+    def on_click(self, cell_cords):
+        pass
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        if cell:
+            self.on_click(cell)
