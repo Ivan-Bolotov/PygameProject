479e4293ead3cddd352f9eb54d7e73d946517d1e
Ivan-Bolotov <ivanbolotov08@inbox.ru>
2024-01-13 18:35:51 +0300

    Client changed.

--- Server/Client.py
+++ Server/Client.py
@@ -1,10 +1,4 @@
 class Client:
-    # def __init__(self, conn, my=((0,) * 10,) * 10, op=((0,) * 10,) * 10):
-    #     self.MyMatrix = list[list[int]](my)
-    #     self.OpponentMatrix = list[list[int]](op)
-    #     self.messages = []
-    #     self.websocket = conn
-
     @staticmethod
     def createRoom(ID):
         return f"ID:{ID}"
@@ -13,29 +7,10 @@ class Client:
     def sendCords(x, y):
         return f"Cords:{x}:{y}"
 
-    async def sendMessage(self, msg):
-        if self.websocket is not None:
-            if not self.websocket.closed:
-                await self.websocket.send(f"Message:{msg}")
-            else:
-                print("Connection is already closed!")
-        else:
-            print("You are still not connected!")
+    @staticmethod
+    def sendMessage(msg):
+        return f"Message:{msg}"
 
-    async def recv(self):
-        if self.websocket is not None:
-            if not self.websocket.closed:
-                return await self.websocket.recv()
-            else:
-                print("Connection is already closed!")
-        else:
-            print("You are still not connected!")
-
-    async def sendRes(self, res):
-        if self.websocket is not None:
-            if not self.websocket.closed:
-                await self.websocket.send(f"Res:{res}")
-            else:
-                print("Connection is already closed!")
-        else:
-            print("You are still not connected!")
+    @staticmethod
+    def sendRes(res):
+        return f"Res:{res}"
