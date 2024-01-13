b8350ab7480099970ca45e049b988bf961003e4a
Ivan-Bolotov <ivanbolotov08@inbox.ru>
2024-01-13 16:22:06 +0300

    Changed.

--- Server/Client.py
+++ Server/Client.py
@@ -1,34 +1,17 @@
-import asyncio
-import websockets as ws
-
-
 class Client:
-    def __init__(self, conn, my=((0,) * 10,) * 10, op=((0,) * 10,) * 10):
-        self.MyMatrix = list[list[int]](my)
-        self.OpponentMatrix = list[list[int]](op)
-        self.messages = []
-        self.websocket = conn
+    # def __init__(self, conn, my=((0,) * 10,) * 10, op=((0,) * 10,) * 10):
+    #     self.MyMatrix = list[list[int]](my)
+    #     self.OpponentMatrix = list[list[int]](op)
+    #     self.messages = []
+    #     self.websocket = conn
 
-    async def createRoom(self, ID):
-        if self.websocket is not None:
-            if not self.websocket.closed:
-                await self.websocket.send(f"ID:{ID}")
-            else:
-                print("Connection is already closed!")
-        else:
-            print("You are still not connected!")
+    @staticmethod
+    def createRoom(ID):
+        return f"ID:{ID}"
 
-    def setConnection(self, websocket):
-        self.websocket = websocket
-
-    async def sendCords(self, x, y):
-        if self.websocket is not None:
-            if not self.websocket.closed:
-                await self.websocket.send(f"Cords:{x}:{y}")
-            else:
-                print("Connection is already closed!")
-        else:
-            print("You are still not connected!")
+    @staticmethod
+    def sendCords(x, y):
+        return f"Cords:{x}:{y}"
 
     async def sendMessage(self, msg):
         if self.websocket is not None:
