b304e61b0de7bead10c5f0010b7a0c514d2061a3
Ivan-Bolotov <ivanbolotov08@inbox.ru>
2024-01-13 17:17:45 +0300

    Server changed.

--- Server/Server.py
+++ Server/Server.py
@@ -12,18 +12,22 @@ async def handler(websocket: ws.WebSocketServerProtocol, addr: str):
 
     try:
         websocket.id = str(counter)
+        websocket.is_in_room = False
         counter += 1
         clients.append(websocket)
         await websocket.send("Your ID:" + str(websocket.id))
         while True:
             message = await websocket.recv()
             t, msg = message.split(":")
-            if t == "ID":
+            if t == "ID" and not websocket.is_in_room:
                 for c in clients:
                     if c.id == msg:
-                        print("Комната создана!")
-                        rooms.append([websocket, c])
-                        break
+                        if not c.is_in_room:
+                            print("Комната создана!")
+                            rooms.append([websocket, c])
+                            websocket.is_in_room = True
+                            c.is_in_room = True
+                            break
             else:
                 for i in rooms:
                     if websocket == i[0]:
