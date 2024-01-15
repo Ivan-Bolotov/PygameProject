0edc13ffb62cb9ac532bbd3064d0f82f0c9072bc
roman <romanroman1802@gmail.com>
2024-01-15 23:53:40 +0300

    неавантюрный коммит

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ class Game:
         self.screen.blit(self.label_with_id, self.label_with_id_rect)
 
         """Поля"""
-        self.matrix_1 = [[[0] for _ in range(10)] for __ in range(10)]
-        self.matrix_2 = [[[0] for _ in range(10)] for __ in range(10)]
+        self.matrix_1 = [[0 for _ in range(10)] for __ in range(10)]
+        self.matrix_2 = [[0 for _ in range(10)] for __ in range(10)]
 
         """Основное меню"""
         self.button_quit = Button(self, self.screen, 'QUIT', 'red', text='Выход')
@@ -156,10 +156,13 @@ class Game:
             send_chan.send(Client.sendReady())
             self.running_one = self.game
             self.checking_one = self.game_check
+            # пересылать матрицу тут
 
         if self.get_message():
             self.running_one = self.game
             self.checking_one = self.game_check
+            # пересылать матрицу тут
+
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             self.group.update(event)
