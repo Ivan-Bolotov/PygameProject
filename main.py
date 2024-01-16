35ce30b2df4fe4448441d5d2fa3efb646d8c0eb9
Ivan-Bolotov <ivanbolotov08@inbox.ru>
2024-01-16 21:50:02 +0300

    Changed.

--- main.py
+++ main.py
@@ -255,7 +255,7 @@ class Game:
 def client_process(recv_ch, queue):
     import threading as th
 
-    host, port = "127.0.0.1", 12345
+    host, port = "26.234.107.47", 12345
     with client.connect(f"ws://{host}:{port}") as conn:
         ID = conn.recv()
         queue.put(ID)
